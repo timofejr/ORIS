e5999ba0547b435f08b3d42d36d8a5c1d11083e8
timofejr <timofejr064@gmail.com>
2025-10-04 09:02:03 +0300

    Small fixes Homework 1

--- Homework1/MiniHttpServer/obj/Debug/net9.0/MiniHttpServer.AssemblyInfo.cs
+++ Homework1/MiniHttpServer/obj/Debug/net9.0/MiniHttpServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6b05413f8e1e9cb9490ce72dd51f2c0626c8d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
